edfb6c9e346c7b32db724b4a29848192f91d65b0
Steopa2001 <moraru495@gmail.com>
2025-07-08 16:18:08 +0200

    feat (es 3): complete

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,15 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+const initialLetter = (arrayWords, letter) => {
+  const filter = arrayWords.filter(word => word[0] === letter);
+  return filter;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
+const result = initialLetter(names, 'A');
 
+console.log(result);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
