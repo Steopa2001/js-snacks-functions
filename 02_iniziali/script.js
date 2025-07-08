8d8fe2bad52f6db310bb3eecccb479103230a5b0
Steopa2001 <moraru495@gmail.com>
2025-07-08 15:32:35 +0200

    feat (es 2): complete

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const newArrayInitial = (arrayNomi) => {
+  const initialWords = arrayNomi.map(nome => nome[0]);
+  return initialWords;
+};
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const result = newArrayInitial(names);
 
+console.log(result);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
