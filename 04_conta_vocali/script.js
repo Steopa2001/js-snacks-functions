d10f475ba6b59a8f148cf39d4f8f11bfc7ccb036
Steopa2001 <moraru495@gmail.com>
2025-07-08 16:34:47 +0200

    feat (es 4): complete

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -7,9 +7,9 @@ const word = 'javascript';
 const contaVocali = (str) => {
   //definisco le vocali da cercare 
   const vocali = ['a', 'e', 'i', 'o', 'u'];
-};
+
 //inizializzo un contatore a 0
-let cout = 0;
+let count = 0;
 
 //creo un ciclo su ogni lettera della stringa 
 for (let i = 0; i < str.length; i++) {
@@ -19,12 +19,17 @@ for (let i = 0; i < str.length; i++) {
   //controllo se la lettera è una vocale 
   if (vocali.includes(letter)) {
     count++;
-  }
+   }
 }
+  //restituisco il numero totale di vocali trovate 
+  return count;
+};
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const result = contaVocali(word);
 
+console.log(`${result} (a, a, i)`);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
